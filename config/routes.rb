070b5dae83547c4cc2e1a5289fc05fe1ef5c5722
Edwin <edwin.h.morris@gmail.com>
2012-02-21 13:00:49 -0500

    updated the visual style, added a card flip effect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cardslap::Application.routes.draw do
   root :to => "decks#index"
   resources :decks, only: [:index, :new, :create, :show] do
-    resources :cards, only: [:new, :create, :show]
+    resources :cards, only: [:new, :create]
   end
 end
