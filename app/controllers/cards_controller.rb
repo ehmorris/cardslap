c8ae17cd5177c57a7c80dd30779966c3601b0e38
Edwin Morris <edwin.h.morris@gmail.com>
2012-02-24 17:31:58 -0500

    working on adding updating to cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -20,4 +20,20 @@ class CardsController < ApplicationController
 
     redirect_to @deck
   end
+
+  def edit
+    @deck = Deck.find(params[:deck_id])
+    @card = @deck.cards.build(params[:card])
+  end
+
+  def update
+    @deck = Deck.find(params[:deck_id])
+    @card = @deck.cards.build(params[:card])
+
+    if @card.update_attributes(params[:card])
+      redirect_to @deck
+    else
+      render :edit
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cardslap::Application.routes.draw do
   root :to => "decks#index"
   resources :decks, only: [:index, :create, :show, :destroy] do
-    resources :cards, only: [:new, :create, :destroy]
+    resources :cards
   end
 end
