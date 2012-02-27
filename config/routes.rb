2e18ce0a51a6a79a64a319a136d7bc392fdb6876
Edwin <edwin.h.morris@gmail.com>
2012-02-26 20:51:53 -0500

    first try at nesting everything under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Cardslap::Application.routes.draw do
   root :to => "decks#index"
-  resources :decks, only: [:index, :create, :show, :destroy, :update, :edit] do
-    resources :cards
+  resources :users do
+    resources :decks, only: [:index, :create, :show, :destroy, :update, :edit] do
+      resources :cards
+    end
   end
 end
