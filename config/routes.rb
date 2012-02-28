23662d9a4b45f32c8a1bcf2c58408584e861b733
Edwin <edwin.h.morris@gmail.com>
2012-02-27 21:51:21 -0500

    added password reset and emailing capabilities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Cardslap::Application.routes.draw do
   root :to => "decks#index"
+
   resources :decks, only: [:index, :create, :show, :destroy, :update, :edit] do
     resources :cards
+  end
+
+  resources :passwords,
+    :controller => 'clearance/passwords',
+    :only       => [:new, :create]
+
+  resources :users, :controller => 'clearance/users', :only => [:new, :create] do
+    resource :password,
+      :controller => 'clearance/passwords',
+      :only       => [:create, :edit, :update]
   end
 end
