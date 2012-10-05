49eca866b8a8789266438d765ff9037112826237
Edwin <edwin.h.morris@gmail.com>
2012-10-04 20:41:33 -0400

    [#47] fix email url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery
+
+  def default_url_options
+    if Rails.env.production?
+      {:host => "http://cardslap.heroku.com"}
+    else
+      {}
+    end
+  end
 end
