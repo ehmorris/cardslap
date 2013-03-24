35a5f07868e1d43fcd434c8f55aa05acddc24ca6
Edwin <edwin.h.morris@gmail.com>
2013-03-23 20:32:14 -0400

    [#50] add links back to deck from quiz and shared screens

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -2,6 +2,7 @@ class QuizController < ApplicationController
   before_filter :authorize
 
   def index
+    @quiz = true
     @user = current_user
     @deck = @user.decks.find(params[:deck_id])
     @cards = @deck.cards
