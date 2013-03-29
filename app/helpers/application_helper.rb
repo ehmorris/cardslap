5298be7f85d41c59d8faa12b0a4150a9c87a6319
Edwin <edwin.h.morris@gmail.com>
2013-03-29 17:21:34 -0300

    better testing for @share element in header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def header_deck_link
-    if (defined? @share and !@share.id.nil?) and defined? @deck
+    if (defined? @share and @share.persisted?) and defined? @deck
       link_to @deck.name, deck_share_path(@deck.id, @share)
     elsif (defined? @card or defined? @quiz) and defined? @deck
       link_to @deck.name, deck_path(@deck)
