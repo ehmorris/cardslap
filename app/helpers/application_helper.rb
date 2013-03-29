3cdad520e899ce1f0d6cde0a813f3d90cb5ee496
Edwin <edwin.h.morris@gmail.com>
2013-03-28 23:44:17 -0400

    Correct breadcrumb navigation, extract to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def header_deck_link
+    if (defined? @share and !@share.id.nil?) and defined? @deck
+      link_to @deck.name, deck_share_path(@deck.id, @share)
+    elsif (defined? @card or defined? @quiz) and defined? @deck
+      link_to @deck.name, deck_path(@deck)
+    elsif defined? @deck
+      @deck.name
+    end
+  end
 end
