ce1754415e39415fb6456a4020327c9ebbe8078a
Edwin <edwin.h.morris@gmail.com>
2013-04-03 11:02:53 -0400

    tweak bin html and css for passable rendering reorganize bin css, hide reorder targets in bin

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,16 +1,16 @@
 $(function() {
   // flip card over on guess and show guess on backside form
-  $('.cards-quiz > li form.form-front').submit(function() {
+  $('.cards-quiz form.form-front').submit(function() {
     $(this).parent().addClass('flip');
     var guess = $(this).children('textarea').val();
-    $(this).parents('li').find('form.form-back .guess').text(guess);
+    $(this).parents('.question').find('form.form-back .guess').text(guess);
     return false;
   });
 
   // memorize or reshuffle card if guess is correct or incorrect
-  $('.cards-quiz > li form.form-back input[type="button"]').click(function() {
+  $('.cards-quiz form.form-back input[type="button"]').click(function() {
     var deck_id = $(this).parents('.cards-quiz').data('deck_id');
-    var card_id = $(this).parents('li').data('id');
+    var card_id = $(this).parents('.question').find('.single-card').data('id');
 
     if ($(this).hasClass('yes')) {
       // put into memorized pile and show next card
@@ -20,13 +20,13 @@ $(function() {
         data: {_method: 'PUT', card: {bin: 'bin'}}
       });
 
-      var html = $(this).parents('li').clone().wrap('<div>').parent().html();
+      var html = $(this).parents('.question').find('.single-card').clone().wrap('<div>').parent().html();
       $('#bin').html($('#bin').html() + html);
     }
 
     // show next if it exists, otherwise show end message
-    if ($(this).parents('li').next().length) {
-      $(this).parents('li').hide().next().show();
+    if ($(this).parents('.question').next().length) {
+      $(this).parents('.question').hide().next().show();
     }
     else {
       $('.cards-quiz').html(
