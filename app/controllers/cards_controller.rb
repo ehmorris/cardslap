fa831ba26f70fb3db50891878ac3eb2c041ef241
Edwin <edwin.h.morris@gmail.com>
2013-04-03 10:33:34 -0400

    created cards are added to the front of a deck sort numbers are changed so that that card always reappears as the first card in the deck on reload

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,11 +2,6 @@ class CardsController < ApplicationController
   helper :all
   before_filter :authorize
 
-  def new
-    @deck = current_user.decks.find(params[:deck_id])
-    @card = Card.new
-  end
-
   def create
     @deck = current_user.decks.find(params[:deck_id])
     @card = @deck.cards.build(params[:card])
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,10 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+  after_initialize :init
+
+  def init
+    self.sort_number ||= 0
+  end
 
   comma do
     front
