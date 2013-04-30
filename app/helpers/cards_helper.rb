64b7b0e8d31ae2df105dab5ac6911811098659c7
Edwin <edwin.h.morris@gmail.com>
2013-04-29 21:05:00 -0400

    better image detection

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,8 +1,14 @@
 module CardsHelper
+  def is_string_image_extension? str
+    return ['jpeg', 'jpg', 'png', 'gif', 'webp'].include? str
+  end
+
   def is_string_image? str
     uri = URI.parse(str)
-    # if the string is a url, assume it's an image
-    return %w(http https).include?(uri.scheme)
+    # if string is a url and ends in an image extension, assume it's an image
+    return %w(http https).include?(uri.scheme) &&
+           is_string_image_extension?(uri.path.rpartition('.').last)
+
   # if URI.parse raises an invalid error, return false
   rescue URI::InvalidURIError
     return false
